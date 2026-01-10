106f27e0e29a08deac3ff5228a870d528f06db79
DoMo <ericdominguezm@gmail.com>
2026-01-11 00:43:11 +0100

    init: initialize user profile service project with Gradle setup and basic application structure

---
+++ src/main/java/com/example/userprofileservicedev/UserProfileServiceDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.userprofileservicedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileServiceDevApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(UserProfileServiceDevApplication.class, args);
+    }
+
+}
